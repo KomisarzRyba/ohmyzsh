ebe8f136940f6ee6bccd7e4298adcb9bde961c20
KomisarzRyba <antek.olesik@gmail.com>
2024-05-15 19:14:04 -0400

    icons

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,4 +1,4 @@
 alias camera="gphoto2 --stdout --capture-movie | ffmpeg -hwaccel nvdec -c:v mjpeg_cuvid -i - -vcodec copy -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0"
-alias ls="eza"
+alias ls="eza --icons"
 alias cat="batcat"
 alias c="clear"
